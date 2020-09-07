3b5a638dc2b88ff7cfb07e9892ab1fcf5db4fb2d
lcshum <melcshum@gmail.com>
2020-09-07 22:23:21 +0800

    add product

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::create([
+            'product_name' => 'MacBook Pro',
+            'product_description' => 'Apple Laptop'
+        ]);
+
+        Product::create([
+            'product_name' => 'Huawei GR5 2017',
+            'product_description' => 'Andriod Smart Phone'
+
+}
