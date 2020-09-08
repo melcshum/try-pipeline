361a44952ca6f0d9e04282f6daa18bb69017d827
lcshum <melcshum@gmail.com>
2020-09-08 08:06:01 +0800

    update redis setting

--- config/database.php
+++ config/database.php
@@ -171,7 +171,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'), //default phpredis
+        'client' => env('REDIS_CLIENT', 'predis'), //default phpredis
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
